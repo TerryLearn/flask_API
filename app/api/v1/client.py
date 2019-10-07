ba634f3c83d9918269d55f2bbf2cc1dbf427f904
Terry <ytdxxt10@163.com>
2019-10-07 20:29:44 +0800

    处理不同客户端的注册

--- app/api/v1/client.py
+++ app/api/v1/client.py
@@ -1,10 +1,26 @@
+from flask import request
+
+from app.libs.enums import ClientTypeEnum
 from app.libs.redprints import Redprint
+from app.validators.forms import ClientForm
 
 api = Redprint('client')
 
 
-@api.route('/register')
+@api.route('/register', methods=['POST'])
 def create_client():
     #注册登录
     #参数校验
+    # WTForms 验证表单
+    #表单 JSON
+    data = request.json
+    form = ClientForm(data=data)
+    if form.validate():
+        promise = {
+            ClientTypeEnum.USER_EMAIL: __register_user_by_email,
+        }
+    pass
+
+
+def __register_user_by_email():
     pass
